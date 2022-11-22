f5f9dee9548d85d1330208f50d957ccab1281af1
Guilherme Moreira <guimoreira.ads@gmail.com>
2022-11-22 20:26:12 -0300

    ControllerContasPagar implementado

--- src/controller/ControllerContasPagar.java
+++ src/controller/ControllerContasPagar.java
@@ -4,10 +4,34 @@
  */
 package controller;
 
-/**
- *
- * @author Usuario
- */
+import DAO.DaoContasPagar;
+import java.util.ArrayList;
+import model.ModelContasPagar;
+
 public class ControllerContasPagar {
+    DaoContasPagar daoContasPagar = new DaoContasPagar();
     
+    public int salvarContasPagarController(ModelContasPagar pModelContasPagar) {
+        return this.daoContasPagar.salvarContasPagarDAO(pModelContasPagar);
+    }
+
+    //recebe requisição de excluir produtos de DAO
+    public boolean excluirContasPagarController(int pCodigo) {
+        return this.daoContasPagar.excluirContasPagarDAO(pCodigo);
+    }
+
+    //recebe requisição de alterar produtos de DAO
+    public boolean alterarContasPagarController(ModelContasPagar pModelContasPagar) {
+        return this.daoContasPagar.alterarContasPagarDAO(pModelContasPagar);
+    }
+    
+    //recebe requisição de retornar produto especifico de DAO
+    public ModelContasPagar retornarContasPagarController(int pCodigo){
+        return this.daoContasPagar.retornarContasPagarDAO(pCodigo);
+    }
+    
+    //recebe requisição de retornar produtos de DAO
+    public ArrayList<ModelContasPagar> retornarListaContasPagarController(){
+        return this.daoContasPagar.retornarListaContasPagarDAO();
+    }
 }
